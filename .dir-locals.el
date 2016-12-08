b77d8f2facf0efd31730f4bc5ac2410d91a2a10f
Thomas Stibor <t.stibor@gsi.de>
2016-12-08 16:16:19 +0100

    Extend flycheck-clang-* settings
    Add tsmapi path to flycheck-clang-include-path and
    preprocessor settings to clear flycheck warnings

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,15 @@
-((nil . ((flycheck-clang-include-path . ("/opt/tivoli/tsm/client/api/bin64/sample")))))
+((nil . (
+	 (flycheck-clang-include-path . (
+					 "."
+					 "/opt/tivoli/tsm/client/api/bin64/sample"
+					 "/home/tstibor/dev/tsm/github/ltsm/src/lib"
+					 ))
+	 (flycheck-clang-definitions . (
+					"_FILE_OFFSET_BITS=64"
+					"_LARGEFILE64_SOURCE"
+					))
+	 (flycheck-clang-language-standard . "gnu99")
+	 (c-basic-offset . 8)
+	 )
+      )
+ )
