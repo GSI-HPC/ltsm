90a0846adc0f2cdf2835b255b35cd3b4452cdadd
Thomas Stibor <t.stibor@gsi.de>
2017-07-18 12:38:04 +0200

    [test] sanity check script ltsmc --pipe archiving
    Simple test script to check --pipe archiving and
    thus testing functions
    
    int tsm_fopen(const char *fs, const char *fpath, const char *desc,
    	      struct login_t *login, struct session_t *session);
    ssize_t tsm_fwrite(const void *ptr, size_t size, size_t nmemb,
    		   struct session_t *session);
    int tsm_fclose(struct session_t *session);

---
+++ script/sanity.ltsmc.1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+source $(dirname $(readlink -f $0))/include.sh
+VERBOSE=1
+
+__check_results()
+{
+    if [ ${1} != 0 ];
+    then
+	echo "${2}"
+	rm -f ${3}
+	exit 1
+    fi
+}
+
+##########################################################
+# settings
+##########################################################
+TSM_NAME=${1-polaris}
+LTSM_BIN="src/ltsmc"
+LTSM_NODE=${TSM_NAME}
+LTSM_PASSWORD=${TSM_NAME}
+LTSM_SERVERNAME=${2-polaris-kvm-tsm-server}
+LTSM_VERBOSE=${3-message}
+LTSM_FS=${4-/}
+
+[ ${PWD##*/} == "script" ] && { LTSM_BIN="../${LTSM_BIN}"; }
+__check_bin "${LTSM_BIN}"
+
+##########################################################
+# main
+##########################################################
+MAX_REPEAT=10
+MAX_KiB=4096
+
+for r in $(seq 1 ${MAX_REPEAT});
+do
+    RND_STR="/tmp/$(__rnd_string 8)"
+
+    dd if=/dev/urandom of=${RND_STR} bs=1KiB count=$(( (RANDOM % ${MAX_KiB}) + 1 )) 2>/dev/null
+    CRC32_ORIG=`${LTSM_BIN} --checksum ${RND_STR} | awk '{print $2}'`
+
+    cat ${RND_STR} | ${LTSM_BIN} --pipe --node ${LTSM_NODE} --password ${LTSM_PASSWORD} \
+    				 --servername ${LTSM_SERVERNAME} --verbose ${LTSM_VERBOSE} ${RND_STR}
+
+    __check_results $? "archive via pipe failed" ${RND_STR}
+
+    CRC32_ARCHIVED=`${LTSM_BIN} --delete --node ${LTSM_NODE} --password ${LTSM_PASSWORD} --servername ${LTSM_SERVERNAME} --verbose info ${RND_STR} 2>&1 | awk '/crc32/{print $3}'`
+
+    __check_results $? "query and delete failed" ${RND_STR}
+
+    rm -f ${RND_STR}
+    
+    if [ ${CRC32_ORIG} != ${CRC32_ARCHIVED} ];
+    then
+    	echo "CRC32 checksum did not match for ${RND_STR}, CRC32_ORIG: ${CRC32_ORIG} != CRC32_ARCHIVED: ${CRC32_ARCHIVED}"
+    	exit 1
+    else
+    	__log "CRC32 checksum matched for ${RND_STR}, CRC32_ORIG: ${CRC32_ORIG} == CRC32_ARCHIVED: ${CRC32_ARCHIVED}"
+    fi
+done
